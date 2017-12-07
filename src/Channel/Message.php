22ddc534456757d2fe5b656111338ab8c9447b1d
Daniël Klabbers <daniel@klabbers.email>
2017-12-07 02:01:10 +0100

    working on the first extension

--- src/Channel/Message.php
+++ src/Channel/Message.php
@@ -18,6 +18,7 @@ use Carbon\Carbon;
 use Discodian\Parts\Part;
 use Discodian\Parts\User\Member;
 use Discodian\Parts\User\User;
+use Illuminate\Support\Collection;
 
 /**
  * A message which is posted to a Discord text channel.
@@ -27,16 +28,16 @@ use Discodian\Parts\User\User;
  * @property string $channel_id
  * @property string $content
  * @property int $type
- * @property Collection[User] $mentions
+ * @property Collection|User[] $mentions
  * @property User $author
  * @property bool $mention_everyone
  * @property Carbon $timestamp
  * @property Carbon|null $edited_timestamp
  * @property bool $tts
  * @property array $attachments
- * @property Collection[Embed] $embeds
+ * @property Collection|Embed[] $embeds
  * @property string|null $nonce
- * @property Collection[Role] $mention_roles
+ * @property Collection|Role[] $mention_roles
  * @property bool $pinned
  */
 class Message extends Part
